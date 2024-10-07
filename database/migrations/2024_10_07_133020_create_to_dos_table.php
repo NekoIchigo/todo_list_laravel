7056fa3606f700813ea6653833cf0bd5b9d5a406
Reydan <reydanjohnbelen@gmail.com>
2024-10-07 21:53:30 +0800

    todos columns

--- database/migrations/2024_10_07_133020_create_to_dos_table.php
+++ database/migrations/2024_10_07_133020_create_to_dos_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('to_dos', function (Blueprint $table) {
             $table->id();
+            $table->foreignId("user_id")->constrained("user");
+            $table->longText("description");
+            $table->string("status");
             $table->timestamps();
         });
     }
