76bb223151baf4e6852d75c16834fbe8ab3a3aee
Reydan <reydanjohnbelen@gmail.com>
2024-10-07 23:47:00 +0800

    api working

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,15 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::post("/login", "AuthController@login");
+Route::post("/login", [AuthController::class, "login"]);
 
 Route::middleware("auth:sanctum")->group(function() {
-
-    Route::post("/logout", "AuthController@logout");
+    Route::apiResources([
+        "users" => UserController::class,
+    ]);
+    Route::post("/logout", [AuthController::class, "logout"]);
 });
