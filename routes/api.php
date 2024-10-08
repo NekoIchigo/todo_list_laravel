65fd0ec7faa96d8514e11bc98fb08545b1de4393
Reydan <reydanjohnbelen@gmail.com>
2024-10-08 10:22:36 +0800

    add todo apu

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ToDoController;
 use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -10,6 +11,7 @@ Route::post("/login", [AuthController::class, "login"]);
 Route::middleware("auth:sanctum")->group(function() {
     Route::apiResources([
         "users" => UserController::class,
+        "todos" => ToDoController::class,
     ]);
     Route::post("/logout", [AuthController::class, "logout"]);
 });
